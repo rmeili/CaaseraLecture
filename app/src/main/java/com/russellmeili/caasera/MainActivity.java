2dc6618e3628084b6dc30b8b00510b1d5aaa1b54
Russ Meili <rmeili69@netscape.net>
2017-01-31 16:29:33 -0800

    Added Login and cancel buttons, and toasted the results

--- app/src/main/java/com/russellmeili/caasera/MainActivity.java
+++ app/src/main/java/com/russellmeili/caasera/MainActivity.java
@@ -2,13 +2,27 @@ package com.russellmeili.caasera;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText edtLoginName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtLoginName = (EditText) findViewById(R.id.edtLoginName);
+    }
 
+    public void btnLoginOnClick(View v) {
+        Log.i("CLICK", "Login button was clicked.");
+        Log.i("CLICK", edtLoginName.getText().toString());
+        Toast.makeText(this, "You successfully Logged in: " + edtLoginName.getText().toString(), Toast.LENGTH_LONG).show();
     }
 }
